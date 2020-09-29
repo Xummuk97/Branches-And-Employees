220b4379fe2a2fa71c8285a25b3b035cf657bd60
Xummuk97 <mr.xummuk97@list.ru>
2020-09-29 23:12:53 +0500

    Исправление вида таблицы при просмотре все филиалов

--- resources/views/branch/index.blade.php
+++ resources/views/branch/index.blade.php
@@ -16,21 +16,21 @@
         </tr>
         </thead>
         <tbody>
-            <tr>
-                @forelse ($branches as $branch)
-                        <th scope="row">{{ $branch->id }}</th>
-                        <td>{{ $branch->name  }}</td>
-                        <td>
-                            <form>
-                                {{ csrf_field() }}
-                                <input type="hidden" name="_id" value="{{ $branch->id }}">
-                                <button type="submit" class="btn btn-primary">Подробнее</button>
-                            </form>
-                        </td>
-                @empty
-                    <td colspan="3">Филиалов нет!</td>
-                @endforelse
-            </tr>
+            @forelse ($branches as $branch)
+                <tr>
+                    <th scope="row">{{ $branch->id }}</th>
+                    <td>{{ $branch->name  }}</td>
+                    <td>
+                        <form>
+                            {{ csrf_field() }}
+                            <input type="hidden" name="_id" value="{{ $branch->id }}">
+                            <button type="submit" class="btn btn-primary">Подробнее</button>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <tr><td colspan="3">Филиалов нет!</td></tr>
+            @endforelse
         </tbody>
     </table>
 @endsection
